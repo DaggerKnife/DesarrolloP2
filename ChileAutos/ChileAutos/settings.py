6c08795af146b04a1b38b2b279552c0d3a021ed9
DaggerKnife <joshuaz295@gmail.com>
2020-10-31 18:52:21 -0300

    Modificacion Base de datos en  Setting

--- ChileAutos/ChileAutos/settings.py
+++ ChileAutos/ChileAutos/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'ChileAutos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Prueba2',
+        'USER': 'root',
+        'PASSWORD': 'duoc' ,
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 

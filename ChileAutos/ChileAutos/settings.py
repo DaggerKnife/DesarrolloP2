596d43012bd0e8fa9f234f7eb21a363693338d5f
DaggerKnife <joshuaz295@gmail.com>
2020-11-03 15:54:16 -0300

    Agregado link de head y scripts y archivos con static

--- ChileAutos/ChileAutos/settings.py
+++ ChileAutos/ChileAutos/settings.py
@@ -129,7 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
-STATIC_ROOT= join(BASE_DIR,'static','static_root')
-STATICFILES_DIRS= [join(BASE_DIR,'static')]
+STATIC_ROOT = join(BASE_DIR,'archivos','static_root')
+STATICFILES_DIRS = [
+    join(BASE_DIR,'archivos')
+]
+## Rutas para media/ archivos que se suben por la pagina...
+MEDIA_URL = '/media/'
+MEDIA_ROOT = join(BASE_DIR,'media')
 
